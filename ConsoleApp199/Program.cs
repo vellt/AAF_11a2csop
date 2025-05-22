0e1f25aedd10c534b7f4fa32a7c02c0f8cba6200
vellt <szantobenjamin97@gmail.com>
2025-05-22 15:21:45 +0200

    mai ora

---
+++ ConsoleApp199/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp199
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> nevek = new List<string>(); //0
+            nevek.Add("Geza");
+            nevek.Add("Béla");
+            nevek.Add("Béla");
+            nevek.Add("Cecil");
+            nevek.Add("Árpád");
+
+            Console.WriteLine($"lista hossza: {nevek.Count()}"); //5
+
+            //van-e benne Árpád
+            bool van = nevek.Contains("Árpád");
+            Console.WriteLine(van ? "igen" : "nincs");
+
+            // hanyadik indexen van Béla
+            int index = nevek.IndexOf("Béla");//ha nincs Béla-->-1
+            Console.WriteLine(index);
+
+            // nevek kiírása foreach-el
+            foreach (string nev in nevek)
+            {
+                Console.WriteLine(nev);
+            }
+
+            for (int i = 0; i < nevek.Count(); i++)
+            {
+                Console.WriteLine(nevek[i]);
+            }
+
+            // ábécé szerint növekvőbe rendezni az elemeket
+            nevek.Sort();
+            Console.WriteLine();
+            // nevek kiírása foreach-el
+            foreach (string nev in nevek)
+            {
+                Console.WriteLine(nev);
+            }
+
+            // ábécé szerint csökkenő sorrendbe tenni
+            nevek.Reverse();
+            Console.WriteLine();
+            // nevek kiírása foreach-el
+            foreach (string nev in nevek)
+            {
+                Console.WriteLine(nev);
+            }
+
+            nevek.RemoveAt(0); // mit csinál?
+            // eltávolítja az első elemet
+
+            nevek.Remove("Béla"); // minden előfordulást töröl
+
+            foreach (string nev in nevek.Distinct())
+            {
+                Console.WriteLine(nev);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
--- ConsoleApp199/Properties/AssemblyInfo.cs
+++ ConsoleApp199/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApp200")]
+[assembly: AssemblyTitle("ConsoleApp199")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp200")]
+[assembly: AssemblyProduct("ConsoleApp199")]
 [assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("fe8c266b-a06f-49be-acdb-04fa5cc7325b")]
+[assembly: Guid("5db1d37d-78a3-4882-8f16-84e939dc1923")]
 
 // Version information for an assembly consists of the following four values:
 //
